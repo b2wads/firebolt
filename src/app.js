c6c1e1f82aaf4a2d742e1da8c13c73f199b7a019
Deborah Miranda Casanova Costa <deborah.costa@b2wdigital.com>
2019-10-09 13:34:45 -0300

    tests: aumenta cobertura de teste

--- src/app.js
+++ src/app.js
@@ -4,13 +4,9 @@ import { Button } from 'grimorio-ui'
 export default class App extends Component {
   render() {
     return (
-      <Button onClick={this.props.onClick} iconLeft="whatshot">
+      <Button onClick={() => window.alert('hello world!')} iconLeft="whatshot">
         hello
       </Button>
     )
   }
-}
-
-App.defaultProps = {
-  onClick: () => window.alert('hello world!'),
 }
--- src/test.spec.js
+++ src/test.spec.js
@@ -2,13 +2,17 @@ import { shallow } from 'enzyme'
 import App from './app'
 import React from 'react'
 
+import { Button } from 'grimorio-ui'
+
 const props = {
   onClick: jest.fn()
 }
 const wrapper = shallow(<App {...props} />)
 
 it('prim', () => {
+  window.alert = jest.fn()
+  jest.spyOn(window, 'alert')
   expect(wrapper.debug()).toMatchSnapshot()
-  wrapper.simulate('click')
-  expect(props.onClick).toHaveBeenCalled()
+  wrapper.find(Button).props().onClick()
+  expect(window.alert).toHaveBeenCalled()
 })

a147a39ea05937470e6e536bdb2937eefafc49bc
Lucas Domingues <lucasdomi@gmail.com>
2019-10-09 15:50:01 -0300

    fix: escreve regras globais para o arquivo do eslint e adiciona o plugin para o import

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
-import React, { Component } from 'react'
+import React, { PureComponent } from 'react'
 import { Button } from 'grimorio-ui'
 
-export default class App extends Component {
+export default class App extends PureComponent {
   render() {
     return (
-      <Button onClick={() => window.alert('hello world!')} iconLeft="whatshot">
-        hello
-      </Button>
+      <>
+        <Button
+          onClick={() => window.alert('hello world!')}
+          iconLeft="whatshot"
+        >
+          hello
+        </Button>
+      </>
     )
   }
 }

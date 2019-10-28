f76161d22d6a31ff44a727835256a158dee70f05
Lucas Domingues <lucasdomi@gmail.com>
2019-10-28 15:51:25 -0300

    refact: ajusta nome do reducer

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux'
 
-import CounterConnect from '../components/CounterConnect/counterConnect-reducer'
+import CounterConnect from '../components/counter-connect/counter-connect-reducer'
 
 export default combineReducers({
-  totalCounter: CounterConnect
+  totalCounter: CounterConnect,
 })
--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -5,6 +5,7 @@ import CounterConnect from '~/components/counter-connect'
 export default function Home() {
   return (
     <>
+      <h1>Exemplos de componentes</h1>
       <CounterConnect />
       <CounterNotConnect />
     </>

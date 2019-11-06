52c5e2c2b698693240d770c651fafc8f91d7ec00
Lucas Domingues <lucasdomi@gmail.com>
2019-11-06 16:19:37 -0300

    test: importa react

--- src/components/meu-componente/meu-componente.spec.js
+++ src/components/meu-componente/meu-componente.spec.js
@@ -1,16 +1,14 @@
-
+import React from 'react'
 import { shallow } from 'enzyme'
-import MeuComponente from './meu-componente-component';
+import MeuComponente from './meu-componente-component'
 
 /** @test {MeuComponente} */
 describe('MeuComponente component', () => {
-/** @test {MeuComponente#render} */
+  /** @test {MeuComponente#render} */
   describe('#render', () => {
     it('render correctly', () => {
-      const wrapper = shallow(
-        <MeuComponente />
-      );
-      expect(wrapper.length).toEqual(1);
-    });
-  });
-});
+      const wrapper = shallow(<MeuComponente />)
+      expect(wrapper.length).toEqual(1)
+    })
+  })
+})
